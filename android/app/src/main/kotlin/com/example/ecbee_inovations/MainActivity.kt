959552f177d411a84d7f0512a3a3be484afd3103
Smohana-Priya <smohanapriya2604@gmail.co>
2024-03-19 17:22:09 +0530

    project setup

---
+++ android/app/src/main/kotlin/com/example/ecbee_inovations/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ecbee_inovations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'screens/login.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Login();
+  }
+}
